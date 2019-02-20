8415be13c55a3093106dc2c79e2a07c39ed85497
Dave Gurnell <dave@untyped.com>
2019-02-20 08:40:02 +0000

    Rename `distance` to `length`

--- src/test/scala/paths/SegmentSpec.scala
+++ src/test/scala/paths/SegmentSpec.scala
@@ -3,11 +3,11 @@ package paths
 import org.scalatest._
 
 class SegmentSpec extends FlatSpec with Matchers {
-  "segment.distance" should "calculate the segment's length" in {
+  "segment.length" should "calculate the segment's length" in {
     val a = Point(1.0, 1.0)
     val b = Point(4.0, 5.0)
     val s = Segment(a, b)
 
-    s.distance should equal(5.0 +- 0.001)
+    s.length should equal(5.0 +- 0.001)
   }
 }
